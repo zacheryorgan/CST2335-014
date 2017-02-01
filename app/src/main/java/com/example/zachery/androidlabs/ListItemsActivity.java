0103241cf12a71c374d8ba0edec3633e544695d3
Zachery <zacheryorgan@hotmail.com>
2017-02-01 15:21:44 -0500

    Added things for lab 3

--- app/src/main/java/com/example/zachery/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/zachery/androidlabs/ListItemsActivity.java
@@ -2,6 +2,7 @@ package com.example.zachery.androidlabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ListItemsActivity extends AppCompatActivity {
 
@@ -9,5 +10,42 @@ public class ListItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onCreate()");
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onPause()");
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onStop()");
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+
+    }
+
 }
--- app/src/main/java/com/example/zachery/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/zachery/androidlabs/LoginActivity.java
@@ -1,18 +1,30 @@
 package com.example.zachery.androidlabs;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.app.PendingIntent.getActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final String ACTIVITY_NAME = "LoginActivity";
+        Log.i(ACTIVITY_NAME, "In onCreate()");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +36,61 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
-}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final String ACTIVITY_NAME = "LoginActivity";
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final String ACTIVITY_NAME = "LoginActivity";
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                final String defaultValue = sharedPref.getString("", "email@domain.com");
+                String email = sharedPref.getString(getString(R.string.email), defaultValue);
+            }
+        });
+
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        final String ACTIVITY_NAME = "LoginActivity";
+        Log.i(ACTIVITY_NAME, "In onPause()");
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        final String ACTIVITY_NAME = "LoginActivity";
+        Log.i(ACTIVITY_NAME, "In onStop()");
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        final String ACTIVITY_NAME = "LoginActivity";
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+
+    }
+
+     }
+
+
+
